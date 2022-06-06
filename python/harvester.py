cd94e3c58cc742037c85076d1ade2cf8ea04aab8
walkeranalyticsllc <dan@walkeranalyticsllc.com>
2022-06-06 17:54:11 -0400

    Added single URL scraper fxn.

--- python/harvester.py
+++ python/harvester.py
@@ -20,8 +20,31 @@ def data_import(dbpath, tbl_name):
     return df
 
 
+def scrape_url(url, xpath):
+    """
+    Takes a single URL, starts a Selenium sesh and scrapes text id'd by give XPath
+    """
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument(
+        "user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:100.0) Gecko/20100101 Firefox/100.0")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option('useAutomationExtension', False)
+    driver = webdriver.Chrome(options=options, service=Service(ChromeDriverManager().install()))
+
+    driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+
+    driver.get(url)
+    scraped_text = driver.find_element(by=By.XPATH, xpath=xpath).text
+
+
 if __name__ == "__main__":
     config = configparser.ConfigParser()
+    # --------------------------------------------
+    # config path for IDE dev
+    config.read('sentiment/config/config.ini')
+    # comment out when dev complete
+    # ---------------------------------------------
     config.read('config/config.ini')
 
     # Read in some required file names and locations
@@ -31,5 +54,4 @@ if __name__ == "__main__":
     guide_df = data_import(dbpath=dbpath,
                            tbl_name=urls_and_dates)
     guide_df.columns = ['URL', 'PUB_DATE', 'GAME_DATE']
-    print(guide_df.head())
 
