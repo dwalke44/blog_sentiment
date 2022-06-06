0425d2ff4ae05d0c03ffdc17c6c8041c2547a1a1
walkeranalyticsllc <dan@walkeranalyticsllc.com>
2022-06-06 11:24:54 -0400

    Added single sitemap fetch fxn

--- python/fetch_urls_from_sitemap.py
+++ python/fetch_urls_from_sitemap.py
@@ -1,6 +1,12 @@
 import sqlite3
+import requests
 import pandas as pd
 import numpy as np
+
+"""
+This script will parse a list of XML files from a website's sitemap to compile a list of URLs for scraping
+Follows the tutorial at: https://www.geeksforgeeks.org/xml-parsing-python/
+"""
 
 
 def fetch_sitemaps(dbpath):
@@ -18,3 +24,11 @@ def fetch_sitemaps(dbpath):
 
     return sitemaps_out
 
+
+def get_links_from_sitemap_xml(sitemap_url):
+    """
+    Parses a single XML file for the URLs embedded therein
+    """
+    payload = requests.get(sitemap_url)
+    content = payload.text
+    return content
