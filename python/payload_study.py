150d1beebf88a168e62161b4202510831b9814ed
walkeranalyticsllc <dan@walkeranalyticsllc.com>
2022-06-10 10:41:53 -0400

    Got a plot of token occurrences by length. Will use this to inform embed process.

--- python/payload_study.py
+++ python/payload_study.py
@@ -1,4 +1,4 @@
-import sqlite3
+import os
 import configparser
 import pandas as pd
 import numpy as np
@@ -12,4 +12,8 @@ config.read(f'sentiment/config/config.ini')
 df = data_import(dbpath=config['DEFAULT']['dbpath'],
                  tbl_name=config['LOCALDB']['output_table_name'])
 
-df.head()
+df.columns = ['index', 'URL', 'PUB_DATE', 'GAME_DATE', 'TOKEN_LEN']
+df.TOKEN_LEN.plot.density(color='blue')
+plt.title('KDE - token lengths')
+plt.show()
+plt.savefig(f'{os.getcwd()}/sentiment/output/plots/kde.png')
