99edcfccf42662b5b1062085ff0bf0eb3723c538
Dan Walker <walker.dan890@gmail.com>
2022-06-22 16:39:12 -0400

    Added first model architecture.

--- python/modeling.py
+++ python/modeling.py
@@ -1,5 +1,7 @@
 import configparser
+import math
 import sqlite3
+import pickle
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -17,6 +19,19 @@ def fetch_all_training_data(dbpath:str, tbl_name:str):
     return df
 
 
+def standardize_targets(targets):
+    """
+    Normalizes via z-score method target game outcomes for prediction
+    """
+    mean = sum(targets)/len(targets)
+    var = sum(pow(x - mean, 2) for x in targets) / len(targets)
+    std_dev = math.sqrt(var)
+    output = []
+    for x in targets:
+        output.append((x-mean)/std_dev)
+
+    return output
+
 if __name__ == "__main__":
     config = configparser.ConfigParser()
     # --------------------------------------------
@@ -31,5 +46,26 @@ if __name__ == "__main__":
 
     input_df = fetch_all_training_data(dbpath=dbpath, tbl_name=input_tbl_name)
     dates = input_df.iloc[:, 301].unique()
+    targets = input_df.iloc[:, 302].unique()
+    y = standardize_targets(targets=targets)
+    # Get vocab pickle
+    vocab_pickle = f"{config['TEXT_OPS']['pickle_jar']}/vocab.pickle"
+    with open(vocab_pickle, 'rb') as inputfile:
+        vocab = pickle.load(inputfile)
 
+    # Put model together
+    # input_features = 300
+    # output_feature = 1
+    # 30 samples per timestep, 15 timesteps in total
+    batch_size = 30
+    timesteps = len(dates)
+    max_len = int(config['TEXT_OPS']['token_len'])
+
+    model = Sequential()
+    model.add(Embedding(10000, 64, input_length=max_len))
+    model.add(Flatten())
+    model.add(Dense(1, activation='linear'))
+    model.compile(optimizer='rmsprop', loss='mape', metrics=['mae'])
+
+    # Train model
     for i in np.arange(0, dates):
